badebfd660075b18b56e9e32b080a7ddb168c18f
nkh97430@gmail.com <nkh97430@gmail.com>
2026-04-15 17:05:52 +0200

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
 import { Body } from './components/Body/Body';
-import {Skills} from './pages/skills/Skills'; // Import de ta nouvelle page
-import { Contact } from './pages/contact/Contact'; // Import de ta page de contact
-//redeploy
+import { Skills } from './pages/skills/Skills';
+import { Contact } from './pages/contact/Contact';
+
 function App() {
   return (
     <Router>
       <Header />
-      
-      {/* UN SEUL bloc Routes pour tout le monde */}
+
       <Routes>
-        {/* Page d'accueil */}
-        <Route path="/" element={<Body />} />
-        
-        {/* Page de compétences */}
-        <Route path="/skills" element={<Skills />} />
-        
-        {/* Page de contact - Maintenant à sa place ! */}
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/*" element={<Body />} />
+        <Route path="/skills/*" element={<Skills />} />
+        <Route path="/contact/*" element={<Contact />} />
       </Routes>
 
       <Footer />
     </Router>
   );
-
 }
+
 export default App;
